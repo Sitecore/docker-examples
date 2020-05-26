0a4e794ad3b21eeec136a9096fb1363d7c013b44
Adam Brauer <adab@sitecore.net>
2020-05-26 16:33:16 -0500

    [378170] Custom xConnect model example.
    * Old "Website" project is now DockerDemo.Website
    * Added DockerDemo.XConnect with custom model / config patch
    * Added Build.Build.XConnect.ModelBuilder to auto-generate xConnect model JSON files during solution (Dockerfile) Build.XConnect.ModelBuilder
    *  xConnect model artifacts (dll, model json, config patches) copied out appropriately for every role in individual runtime Dockerfiles

---
+++ custom-images/src/Build.XConnect.ModelBuilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using DockerDemo.XConnect.Model;
+using Sitecore.XConnect.Schema;
+
+namespace Build.XConnect.ModelBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = GetOutputPath(args);
+            SerializeModel(path, DemoModel.Model);
+        }
+
+        private static string GetOutputPath(string[] args)
+        {
+            return args.Length > 0 ? Directory.CreateDirectory(args[0]).FullName : Directory.GetCurrentDirectory();
+        }
+
+        private static void SerializeModel(string path, XdbModel model)
+        {
+            var fileName = model + ".json";
+            var filePath = Path.Combine(path, fileName);
+            var json = Sitecore.XConnect.Serialization.XdbModelWriter.Serialize(model);
+            File.WriteAllText(filePath, json);
+            Console.WriteLine($"Serialized Xdb model {filePath}");
+        }
+    }
+}
---
+++ custom-images/src/DockerDemo.XConnect/Model/DemoFacet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Sitecore.XConnect;
+
+namespace DockerDemo.XConnect.Model
+{
+    [Serializable]
+    [FacetKey(DefaultFacetKey)]
+    public class DemoFacet : Sitecore.XConnect.Facet
+    {
+        public const string DefaultFacetKey = "DemoFacet";
+
+        public string Region { get; set; }
+    }
+}
