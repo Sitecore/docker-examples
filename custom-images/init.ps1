0f2dc5395e6315cdc02a2d9fe81bd6ceb11ed5be
Adam Brauer <adab@sitecore.net>
2020-07-01 14:40:56 -0500

    Use built-in mkcert if present

--- custom-images/init.ps1
+++ custom-images/init.ps1
@@ -91,17 +91,21 @@ Set-DockerComposeEnvFileVariable "SITECORE_LICENSE" -Value (ConvertTo-Compressed
 
 Push-Location docker\traefik\certs
 try {
-    if (!(Test-Path mkcert.exe)) {
+    $mkcert = ".\mkcert.exe"
+    if ($null -ne (Get-Command mkcert.exe -ErrorAction SilentlyContinue)) {
+        # mkcert installed in PATH
+        $mkcert = "mkcert"
+    } elseif (-not (Test-Path $mkcert)) {
         Write-Host "Downloading and installing mkcert certificate tool..." -ForegroundColor Green 
         Invoke-WebRequest "https://github.com/FiloSottile/mkcert/releases/download/v1.4.1/mkcert-v1.4.1-windows-amd64.exe" -UseBasicParsing -OutFile mkcert.exe
         if ((Get-FileHash mkcert.exe).Hash -ne "1BE92F598145F61CA67DD9F5C687DFEC17953548D013715FF54067B34D7C3246") {
             Remove-Item mkcert.exe -Force
             throw "Invalid mkcert.exe file"
         }
-        .\mkcert.exe -install
     }
     Write-Host "Generating Traefik TLS certificate..." -ForegroundColor Green
-    .\mkcert.exe "*.$($HostName).localhost"
+    & $mkcert -install
+    & $mkcert "*.$($HostName).localhost"
 }
 catch {
     Write-Host "An error occurred while attempting to generate TLS certificate: $_" -ForegroundColor Red
--- getting-started/init.ps1
+++ getting-started/init.ps1
@@ -76,18 +76,22 @@ Set-DockerComposeEnvFileVariable "SITECORE_LICENSE" -Value (ConvertTo-Compressed
 
 Push-Location traefik\certs
 try {
-    if (!(Test-Path mkcert.exe)) {
+    $mkcert = ".\mkcert.exe"
+    if ($null -ne (Get-Command mkcert.exe -ErrorAction SilentlyContinue)) {
+        # mkcert installed in PATH
+        $mkcert = "mkcert"
+    } elseif (-not (Test-Path $mkcert)) {
         Write-Host "Downloading and installing mkcert certificate tool..." -ForegroundColor Green 
-        Invoke-WebRequest https://github.com/FiloSottile/mkcert/releases/download/v1.4.1/mkcert-v1.4.1-windows-amd64.exe -UseBasicParsing -OutFile mkcert.exe
+        Invoke-WebRequest "https://github.com/FiloSottile/mkcert/releases/download/v1.4.1/mkcert-v1.4.1-windows-amd64.exe" -UseBasicParsing -OutFile mkcert.exe
         if ((Get-FileHash mkcert.exe).Hash -ne "1BE92F598145F61CA67DD9F5C687DFEC17953548D013715FF54067B34D7C3246") {
             Remove-Item mkcert.exe -Force
             throw "Invalid mkcert.exe file"
         }
-        .\mkcert.exe -install
     }
-    Write-Host "Generating Traefik TLS certificates..." -ForegroundColor Green 
-    .\mkcert.exe -cert-file xp0cm.localhost.crt -key-file xp0cm.localhost.key "xp0cm.localhost"
-    .\mkcert.exe -cert-file xp0id.localhost.crt -key-file xp0id.localhost.key "xp0id.localhost"
+    Write-Host "Generating Traefik TLS certificates..." -ForegroundColor Green
+    & $mkcert -install
+    & $mkcert -cert-file xp0cm.localhost.crt -key-file xp0cm.localhost.key "xp0cm.localhost"
+    & $mkcert -cert-file xp0id.localhost.crt -key-file xp0id.localhost.key "xp0id.localhost"
 }
 catch {
     Write-Host "An error occurred while attempting to generate TLS certificates: $_" -ForegroundColor Red
