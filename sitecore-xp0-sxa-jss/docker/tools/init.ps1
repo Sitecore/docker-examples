047d2a2f88b6099e32fb718e75b844c9876b6a27
Kamruz Jaman <kj@konabos.com>
2021-02-25 23:09:21 -0500

    Updated XP0 SXA+JSS instance to 10.1

--- sitecore-xp0-sxa-jss/docker/tools/init.ps1
+++ sitecore-xp0-sxa-jss/docker/tools/init.ps1
@@ -39,7 +39,7 @@ if (-not $SitecoreGallery) {
     $SitecoreGallery = Get-PSRepository -Name SitecoreGallery
 }
 # Install and Import SitecoreDockerTools 
-$dockerToolsVersion = "10.0.5"
+$dockerToolsVersion = "10.1.4"
 Remove-Module SitecoreDockerTools -ErrorAction SilentlyContinue
 if (-not (Get-InstalledModule -Name SitecoreDockerTools -RequiredVersion $dockerToolsVersion -ErrorAction SilentlyContinue)) {
     Write-Host "Installing SitecoreDockerTools..." -ForegroundColor Green
@@ -55,38 +55,41 @@ Import-Module SitecoreDockerTools -RequiredVersion $dockerToolsVersion
 Write-Host "Populating required .env file variables..." -ForegroundColor Green
 
 # COMPOSE_PROJECT_NAME
-Set-DockerComposeEnvFileVariable "COMPOSE_PROJECT_NAME" -Value $HostName -Path $Path
+Set-EnvFileVariable "COMPOSE_PROJECT_NAME" -Value $HostName -Path $Path
 
 # SITECORE_ADMIN_PASSWORD
-#Set-DockerComposeEnvFileVariable "SITECORE_ADMIN_PASSWORD" -Value $SitecoreAdminPassword -Path $Path
+#Set-EnvFileVariable "SITECORE_ADMIN_PASSWORD" -Value $SitecoreAdminPassword -Path $Path
 
 # SQL_SA_PASSWORD
-Set-DockerComposeEnvFileVariable "SQL_SA_PASSWORD" -Value $SqlSaPassword -Path $Path
+Set-EnvFileVariable "SQL_SA_PASSWORD" -Value $SqlSaPassword -Path $Path
 
 # CD_HOST
-Set-DockerComposeEnvFileVariable "CD_HOST" -Value "cd.$($FullHostName)" -Path $Path
+Set-EnvFileVariable "CD_HOST" -Value "cd.$($FullHostName)" -Path $Path
 
 # CM_HOST
-Set-DockerComposeEnvFileVariable "CM_HOST" -Value "cm.$($FullHostName)" -Path $Path
+Set-EnvFileVariable "CM_HOST" -Value "cm.$($FullHostName)" -Path $Path
 
 # ID_HOST
-Set-DockerComposeEnvFileVariable "ID_HOST" -Value "id.$($FullHostName)" -Path $Path
+Set-EnvFileVariable "ID_HOST" -Value "id.$($FullHostName)" -Path $Path
 
 # REPORTING_API_KEY = random 64-128 chars
-Set-DockerComposeEnvFileVariable "REPORTING_API_KEY" -Value (Get-SitecoreRandomString 64 -DisallowSpecial) -Path $Path
+Set-EnvFileVariable "REPORTING_API_KEY" -Value (Get-SitecoreRandomString 64 -DisallowSpecial) -Path $Path
 
 # TELERIK_ENCRYPTION_KEY = random 64-128 chars
-Set-DockerComposeEnvFileVariable "TELERIK_ENCRYPTION_KEY" -Value (Get-SitecoreRandomString 128) -Path $Path
+Set-EnvFileVariable "TELERIK_ENCRYPTION_KEY" -Value (Get-SitecoreRandomString 128) -Path $Path
+
+# MEDIA_REQUEST_PROTECTION_SHARED_SECRET = random 64 chars
+Set-EnvFileVariable "MEDIA_REQUEST_PROTECTION_SHARED_SECRET" -Value (Get-SitecoreRandomString 64 -DisallowSpecial) -Path $Path
 
 # SITECORE_IDSECRET = random 64 chars
-Set-DockerComposeEnvFileVariable "SITECORE_IDSECRET" -Value (Get-SitecoreRandomString 64 -DisallowSpecial) -Path $Path
+Set-EnvFileVariable "SITECORE_IDSECRET" -Value (Get-SitecoreRandomString 64 -DisallowSpecial) -Path $Path
 
 # SITECORE_ID_CERTIFICATE
 $idCertPassword = Get-SitecoreRandomString 12 -DisallowSpecial
-Set-DockerComposeEnvFileVariable "SITECORE_ID_CERTIFICATE" -Value (Get-SitecoreCertificateAsBase64String -DnsName "localhost" -Password (ConvertTo-SecureString -String $idCertPassword -Force -AsPlainText)) -Path $Path
+Set-EnvFileVariable "SITECORE_ID_CERTIFICATE" -Value (Get-SitecoreCertificateAsBase64String -DnsName "localhost" -Password (ConvertTo-SecureString -String $idCertPassword -Force -AsPlainText)) -Path $Path
 
 # SITECORE_ID_CERTIFICATE_PASSWORD
-Set-DockerComposeEnvFileVariable "SITECORE_ID_CERTIFICATE_PASSWORD" -Value $idCertPassword -Path $Path
+Set-EnvFileVariable "SITECORE_ID_CERTIFICATE_PASSWORD" -Value $idCertPassword -Path $Path
 
 ##################################
 # Configure TLS/HTTPS certificates
